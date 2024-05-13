8f873ce5a1a68b65710e83d474022ff08a9ae719
adeaye <adeaye33@gmail.com>
2024-05-13 20:43:53 +0700

    first deploy

---
+++ src/fetcher.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export const fetchData = async () => {
+    try {
+        const response = await axios.get('https://webdev-be.vercel.app/products');
+        return response.data.data;
+
+    } catch (error) {
+        throw error;
+    }
+}
